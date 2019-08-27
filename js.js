87a4c68ee7b0de0c95fa0ee2c126aa8562a8f9af
SerDmitriy <42520115+SerDmitriy@users.noreply.github.com>
2019-08-27 10:26:56 +0300

    Version 0.3
    27.08.19

--- js.js
+++ js.js
@@ -4,55 +4,50 @@ let counterQuestions = 0;
 let counterCorrectQuestions = 0;
 let mainDb = [
     {
-        number: 1,
         question: "What is the description of NaN? ",
-        answer1: "Not a Null",
-        answer2: "Nothing else Needed",
-        answer3: "Not a Number",
-        answer4: "Negotiate a Number",
+        answers: ["Not a Null",
+            "Nothing else Needed",
+            "Not a Number",
+            "Negotiate a Number"
+        ],
         rightAnswer: "Not a Number",
-        showed: false
     },
     {
-        number: 2,
         question: "Find incorrect option to empty an array in JavaScript?",
-        answer1: "By assigning an empty array: array = array.clear();",
-        answer2: "By assigning array length to 0: array.length = 0;",
-        answer3: "By popping the elements of the array: while(array.length > 0) { array.pop(); }",
-        answer4: "By using the splice array function: array.splice(0, array.length);",
+        answers: ["By assigning an empty array: array = array.clear();",
+            "By assigning array length to 0: array.length = 0;",
+            "By popping the elements of the array: while(array.length > 0) { array.pop(); }",
+            "By using the splice array function: array.splice(0, array.length);"
+        ],
         rightAnswer: "By assigning an empty array: array = array.clear();",
-        showed: false
     },
     {
-        number: 3,
         question: "What way is incorrect to create an array in JS?",
-        answer1: "By creating instance of an array: var someArray = new Array(); ",
-        answer2: "By using an array function: var someArray = function Array(‘value1’, ‘value2’,…, ‘valueN’);",
-        answer3: "By using an array constructor: var someArray = new Array(‘value1’, ‘value2’,…, ‘valueN’); ",
-        answer4: "By using an array literal: var someArray = [value1, value2,…., valueN];",
+        answers: ["By creating instance of an array: var someArray = new Array(); ",
+            "By using an array function: var someArray = function Array(‘value1’, ‘value2’,…, ‘valueN’);",
+            "By using an array constructor: var someArray = new Array(‘value1’, ‘value2’,…, ‘valueN’); ",
+            "By using an array literal: var someArray = [value1, value2,…., valueN];"
+        ],
         rightAnswer: "By using an array function: var someArray = function Array(‘value1’, ‘value2’,…, ‘valueN’);",
-        showed: false
     },
     {
-        number: 4,
         question: "What is a result of using typeof null?",
-        answer1: "Null",
-        answer2: "Number",
-        answer3: "Function",
-        answer4: "Object",
+        answers: ["Null",
+            "Number",
+            "Function",
+            "Object"
+        ],
         rightAnswer: "Object",
-        showed: false
     }
 ];
 
 document.getElementById("submit-btn").onclick = nextQuestion;
+
 const questionText = document.getElementById("question");
-const firstOptionText = document.getElementById("first-option-text");
-const secondOptionText = document.getElementById("second-option-text");
-const thirdOptionText = document.getElementById("third-option-text");
-const fourthOptionText = document.getElementById("fourth-option-text");
 const progressValue = document.getElementById("progress-value");
+
 const answerCheckboxes = document.getElementsByName("question-form__answer-checkbox");
+const answersText = document.getElementsByClassName("question-form__answer-text");
 
 window.onload = initialize;
 
@@ -73,26 +68,11 @@ function initialize() {
     counterCorrectQuestions = 0;
 
     for (let i = 0; i < mainDb.length; i++) {  // random sort question objects
-        mainDb.sort((a, b) => Math.round(Math.random()) - 0.5);
+        mainDb.sort((a, b) => 0.5 - Math.random());
     }
 
     for (let i = 0; i < mainDb.length; i++) {  // random sort answers in objects
-        let newQuestionObj = [];                //need to rewrite
-        for (let j = 1; j <= 4; j++) {
-            newQuestionObj.push(mainDb[i]["answer" + j]);
-        }
-
-        //console.dir(newQuestionObj);
-        for (let j = 1; j <= 10; j++) {
-            newQuestionObj.sort(() => Math.round(Math.random()) - 0.5);
-        }
-        //console.dir(newQuestionObj);
-
-        //console.dir(mainDb[i]);
-        for (let j = 0; j <= 3; j++) {
-            mainDb[i]["answer" + 0 + (j + 1)] = newQuestionObj[j];
-        }
-        //console.dir(mainDb[i]);
+        mainDb[i].answers.sort(() => 0.5 - Math.random());
     }
 
     display();
@@ -126,10 +106,9 @@ function display() {
 
     questionText.innerText = `${counterQuestions + 1}. ${mainDb[counterQuestions].question}`;
 
-    firstOptionText.innerText = `${mainDb[counterQuestions].answer01}`;
-    secondOptionText.innerText = `${mainDb[counterQuestions].answer02}`;
-    thirdOptionText.innerText = `${mainDb[counterQuestions].answer03}`;
-    fourthOptionText.innerText = `${mainDb[counterQuestions].answer04}`;
+    for (let i = 0; i < answersText.length; i++) {
+        answersText[i].innerText = `${mainDb[counterQuestions].answers[i]}`;
+    }
 
     progressValue.value = counterQuestions;
 }
