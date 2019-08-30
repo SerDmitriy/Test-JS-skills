033056d691e2075f98a370060fe57c02ffd3d597
SerDmitriy <42520115+SerDmitriy@users.noreply.github.com>
2019-08-30 16:49:38 +0300

    some task

--- registration.js
+++ registration.js
@@ -1,52 +1,105 @@
 'use strict';
-const mainDb = [
-  ['div', 'Registration form', 'text-primary', null],
-  ['input', 'First name', 'text-info', 'firstName'],
-  ['input', 'Last name', 'input-field', 'lastName'],
-  ['input', 'Email', 'input-field', 'email'],
-  ['input', 'Password', 'password', 'password'],
-  ['input', 'Confirm Password', 'password', 'passwordComfirm'],
-  ['button', 'Register', 'button' ,'registration']
-];
-const main = document.getElementById('main');
 
-const createElement = (elem, text, className, id) => {
-  let el = document.createElement(elem);
-  el.id = id;
-  el.className = className;
-  if (elem === 'div' || 'button') {el.innerText = text}
-  if (elem === 'input') {
-    el.className += ' text-info';
-    el.placeholder = text;
-      el.style.display = 'block';
-  }
-  if (className === 'password' || 'button') {
-    el.type = className;
-    if (className === 'button') {
-      el.className += ' btn btn-success btn-md'
-      el.type = 'submit'
-    }
-  }
-  main.appendChild(el);
-};
+const main = document.getElementById('main');
+let db = {};
 
 const registration = e => {
   e.preventDefault();
+  console.dir(e)
+  for (let i in e.target) {
+    if (i.localName === 'input'){ console.dir(i.value) }
+  }
 
-  let a = [];
-  console.log('registration text ');
-  mainDb.forEach( item => {
-    if (item[0] === 'input') {a.push(document.getElementById(item[3]).value)};
-    console.log(document.getElementById(item[3]).value);
-    return
-  });
-  if (password.value != passwordComfirm.value) {alert ('Passwords not the same')}
-};
-
-const initialize = () => {
-  mainDb.forEach(item => createElement(...item));
-  // document.getElementById('registration').onClick = registration;
+  //check fields on epmty
+  //password compare
 }
 
-initialize();
+const validateName = item => {
+  return item.length < 3 ? 'Name should contain minimum 3 signs' : null
+}
+
+const validateEmail = item => {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+  return re.test(String(item).toLowerCase()) ? 'Your email is incorrect' : null
+}
+
+const validateDate = item => {
+  let nowDate = new Date();
+  console.log(nowDate)
+  console.log(item)
+  let diff = nowDate.getFullYear() - item.getFullYear();
+  return diff < 18 ? 'You are too yung' : null
+}
+
+const toggleErrMsgField = () => {}
+
+const validatePassword = item => {
+  return item.length < 8 ? 'Password should contain minimum 8 signs' : null
+}
+
+const handleChange = input => {
+  let { name, value } = input
+  // console.dir(input)
+  if (name === 'firstName' || 'lastName') {
+    const errorNameMsg = validateName(value)
+    if(errorNameMsg) {
+
+
+      const el = input.nextElementSibling
+      el.removeAttribute("style")
+
+      console.log('el', el)
+
+      // input.nextElementSibling.removeAttribute('hidden')
+      // input.nextElementSibling.innerText = errorNameMsg
+      // input.setAttribute('class', 'error') 
+      
+      // console.dir(input)
+      // console.dir(input.className)
+
+      // if (input.className.indexOf(' error') < 0) {
+      //   input.className += ' error'   
+      // }
+    } else {
+      // input.className = input.className.replace(' error', '')
+      // input.nextElementSibling.hidden = 'true'
+    }
+  }
+  // if (name === 'email') {
+  //   const errorEmailMsg = validateEmail(value)
+  //   if (errorEmailMsg) {
+  //     input.nextElementSibling.hidden = false
+  //     input.nextElementSibling.innerText = errorEmailMsg
+  //     input.className += ' error'   
+  //   } else {
+  //     input.className = input.className.replace(' error', '')
+  //     input.nextElementSibling.hidden = true
+  //   }
+  // }
+  // if (name === 'date') {
+  //   const errorDateMsg = validateDate(value)
+  //   if (errorDateMsg) {
+  //     input.nextElementSibling.hidden = false
+  //     input.nextElementSibling.innerText = errorDateMsg
+  //     input.className += ' error'   
+  //   } else {
+  //     input.className = input.className.replace(' error', '')
+  //     input.nextElementSibling.hidden = true
+  //   }
+  // }
+  //if (name === 'password' || 'confirmPassword' &&  !errorPasswordMsg) {
+  // if (name === 'password' || 'confirmPassword') {
+  //   const errorPasswordMsg = validatePassword(value)
+  //   if (!errorPasswordMsg) {
+  //     input.nextElementSibling.hidden = false
+  //     input.nextElementSibling.innerText = errorPasswordMsg
+  //     input.className += ' error'
+  //   } else {
+  //     input.className = input.className.replace(' error', '')
+  //     input.nextElementSibling.hidden = true
+  //   } 
+  // }
+  db[name] = value
+}
+
 main.addEventListener("submit", registration)
